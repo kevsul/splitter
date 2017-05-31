1519d76036d2ce755a9c87366a4c769d671d5b66
Kevin Sullivan <kevin.sullivan@rakuten.com>
2017-05-31 21:17:01 +0200

    Deploy new Splitter before each test Don't call toNumber() on balances

--- test/splitter.js
+++ test/splitter.js
@@ -1,6 +1,15 @@
 var Splitter = artifacts.require("./Splitter.sol");
 
 contract('Splitter', function(accounts) {
+  let splitter;
+
+  beforeEach("deploy and prepare", function() {
+    return Splitter.new(accounts[1], accounts[2])
+        .then(function(instance) {
+          splitter = instance;
+        });
+  });
+
   it("should create the contract with 2 receivers", async function() {
     const instance = await Splitter.deployed();
     const receiver1 = await instance.receiver1.call();
@@ -10,7 +19,6 @@ contract('Splitter', function(accounts) {
   });
 
   it("should send split eth between the 2 receivers", async function() {
-    const splitter = await Splitter.deployed();
 
     var amount = 40000;
 
@@ -19,25 +27,15 @@ contract('Splitter', function(accounts) {
     const receiver1 = await splitter.receiver1.call();
     const receiver2 = await splitter.receiver2.call();
 
-    const funder_starting_balance = await web3.eth.getBalance(funder).toNumber();
-    const receiver1_starting_balance = await web3.eth.getBalance(receiver1).toNumber();
-    const receiver2_starting_balance = await web3.eth.getBalance(receiver2).toNumber();
-    console.log(`funder_starting_balance = ${funder_starting_balance}`);
-    console.log(`receiver1_starting_balance = ${receiver1_starting_balance}`);
-    console.log(`receiver2_starting_balance = ${receiver2_starting_balance}`);
+    const receiver1_starting_balance = await web3.eth.getBalance(receiver1);
+    const receiver2_starting_balance = await web3.eth.getBalance(receiver2);
 
-    const ret = await splitter.splitFunds.sendTransaction({from: funder, value: amount});
-    console.log(`Split funds, ret = ${ret}`);
+    await splitter.splitFunds.sendTransaction({from: funder, value: amount});
 
-    const funder_ending_balance = await web3.eth.getBalance(funder).toNumber();
-    const receiver1_ending_balance = await web3.eth.getBalance(receiver1).toNumber();
-    const receiver2_ending_balance = await web3.eth.getBalance(receiver2).toNumber();
-    console.log(`funder_ending_balance = ${funder_ending_balance}`);
-    console.log(`funder_ending_balance = ${funder_ending_balance}`);
-    console.log(`receiver1_ending_balance = ${receiver1_ending_balance}`);
-    console.log(`receiver2_ending_balance = ${receiver2_ending_balance}`);
+    const receiver1_ending_balance = await web3.eth.getBalance(receiver1);
+    const receiver2_ending_balance = await web3.eth.getBalance(receiver2);
 
-    assert.equal(receiver1_ending_balance, receiver1_starting_balance + amount / 2, "Amount wasn't correctly split to receiver1");
-    assert.equal(receiver2_ending_balance, receiver2_starting_balance + amount / 2, "Amount wasn't correctly split to receiver1");
+    assert.deepEqual(receiver1_ending_balance, receiver1_starting_balance.plus(amount/2), "Amount wasn't correctly split to receiver1");
+    assert.deepEqual(receiver2_ending_balance, receiver2_starting_balance.plus(amount/2), "Amount wasn't correctly split to receiver1");
   });
 });
